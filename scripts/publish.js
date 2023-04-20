a88d1d79b1d5b22c6225240f766d50ddc1aa62c5
cmsamouco <CarlosSamouco@criticalmanufacturing.com>
2023-04-20 21:17:06 +0100

    fix: publish script to add requried angular cli version

--- scripts/publish.js
+++ scripts/publish.js
@@ -6,18 +6,24 @@ const { argv } = require("process");
 const tag = argv[2];
 
 if (!tag) {
-    throw new Error('Missing tag argument');
+    console.error('Error: Missing tag argument.');
+    process.exit(1);
 }
 
 const rootDir = join(__dirname, '..');
 const packagesDir = join(rootDir, 'packages');
 const packages = readdirSync(packagesDir);
+const rootPackJson = JSON.parse(readFileSync(join(rootDir, 'package.json'), { encoding: 'utf8' }));
+const angularVersion = rootPackJson.dependencies['@schematics/angular'].replace(/^(.\d+)\.\d+\.\d+/, '$1.0.0');
 
 packages.forEach(pack => {
     const packJson = JSON.parse(readFileSync(join(packagesDir, pack, 'package.json'), { encoding: 'utf8' }));
-    const rootPackJson = JSON.parse(readFileSync(join(rootDir, 'package.json'), { encoding: 'utf8' }));
 
     packJson.version = rootPackJson.version;
+
+    packJson.peerDependencies ??= {};
+    packJson.peerDependencies = { "@angular/cli": angularVersion, ...packJson.peerDependencies }
+
     delete packJson.scripts;
 
     Object.keys(packJson.dependencies).forEach((dep) => {
