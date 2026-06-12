ebe5b22b66a1f8a262b38bafc27dba90bd274c42
Carlos Samouco <carlossamouco@criticalmanufacturing.com>
2026-06-12 15:08:52 +0100

    feat: update Node.js version to 24 and improve publish scripts

--- scripts/publish.js
+++ scripts/publish.js
@@ -1,53 +1,68 @@
-const concurrently = require("concurrently");
-const { readFileSync, writeFileSync } = require("fs");
-const { join } = require("path");
-const { argv } = require("process");
-const { getRootPackageJson, getPackages, getPackagesDir } = require("./utils");
+// @ts-check
+const concurrently = require('concurrently').default;
+const { readFileSync, writeFileSync } = require('fs');
+const { join } = require('path');
+const { argv } = require('process');
+const { getRootPackageJson, getPackages, getPackagesDir, generateTag } = require('./utils');
 
 const args = argv.slice(2);
 const dryRun = args.includes('--dry-run');
+const tagIndex = args.indexOf('--tag');
+const tagOverride = tagIndex !== -1 ? args[tagIndex + 1] : null;
 
 const rootPackJson = getRootPackageJson();
 const packages = getPackages();
 const packagesDir = getPackagesDir();
-const angularVersion = rootPackJson.dependencies['@schematics/angular'].replace(/^(.\d+)\.\d+\.\d+/, '$1.0.0');
-packages.forEach(pack => {
-    const packJson = JSON.parse(readFileSync(join(packagesDir, pack, 'package.json'), { encoding: 'utf8' }));
+const angularVersion = rootPackJson.dependencies['@schematics/angular'].replace(
+  /^(.\d+)\.\d+\.\d+/,
+  '$1.0.0'
+);
+packages.forEach((pack) => {
+  const packJson = JSON.parse(
+    readFileSync(join(packagesDir, pack, 'package.json'), { encoding: 'utf8' })
+  );
 
-    packJson.version = rootPackJson.version;
-    packJson.peerDependencies ??= {};
-    packJson.peerDependencies = { "@angular/cli": angularVersion, ...packJson.peerDependencies }
+  packJson.version = rootPackJson.version;
+  packJson.peerDependencies ??= {};
+  packJson.peerDependencies = { '@angular/cli': angularVersion, ...packJson.peerDependencies };
 
-    delete packJson.scripts;
+  delete packJson.scripts;
 
-    Object.keys(packJson.dependencies).forEach((dep) => {
-        if (packages.some(pack => dep === '@criticalmanufacturing/' + pack)) {
-            packJson.dependencies[dep] = rootPackJson.version;
-            return;
-        }
+  Object.keys(packJson.dependencies).forEach((dep) => {
+    if (packages.some((pack) => dep === '@criticalmanufacturing/' + pack)) {
+      packJson.dependencies[dep] = rootPackJson.version;
+      return;
+    }
 
-        if (!rootPackJson.dependencies[dep]) {
-            throw new Error('Unable to get root dependency ', dep);
-        }
+    if (!rootPackJson.dependencies[dep]) {
+      throw new Error('Unable to get root dependency ' + dep);
+    }
 
-        packJson.dependencies[dep] = rootPackJson.dependencies[dep];
-    });
+    packJson.dependencies[dep] = rootPackJson.dependencies[dep];
+  });
 
-    writeFileSync(join(packagesDir, pack, 'package.json'), JSON.stringify(packJson, null, 2));
+  writeFileSync(join(packagesDir, pack, 'package.json'), JSON.stringify(packJson, null, 2));
 });
 
 (async () => {
-    // Publish packages
-    const publishCmd = packages.map(pack => 
-        `npm publish ${join(packagesDir, pack)} --registry https://registry.npmjs.org/${dryRun ? ' --dry-run' : ''}`
-    );
-    
-    if (dryRun) {
-        console.log('[DRY RUN] Would publish packages');
-    }
-    
-    console.log('Running publish commands:');
-    publishCmd.forEach(cmd => console.log(`  ${cmd}`));
-    
-    await concurrently(publishCmd, { raw: true }).result;
+  // Derive npm dist-tag: use CLI override, then auto-detect from pre-release version
+  const version = rootPackJson.version;
+  const isPrerelease = version.includes('-');
+  const resolvedTag = tagOverride ?? (isPrerelease ? generateTag(version) : null);
+  const tagArg = resolvedTag ? ` --tag ${resolvedTag}` : '';
+
+  // Publish packages
+  const publishCmd = packages.map(
+    (pack) =>
+      `npm publish ${join(packagesDir, pack)}${tagArg} --registry https://registry.npmjs.org/${dryRun ? ' --dry-run' : ''}`
+  );
+
+  if (dryRun) {
+    console.log('[DRY RUN] Would publish packages');
+  }
+
+  console.log('Running publish commands:');
+  publishCmd.forEach((cmd) => console.log(`  ${cmd}`));
+
+  await concurrently(publishCmd, { raw: true }).result;
 })();
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,46 +1,48 @@
-const { readdirSync, readFileSync } = require("fs");
-const { join } = require("path");
+const { readdirSync, readFileSync } = require('fs');
+const { join } = require('path');
 
 function getRootDir() {
-    return join(__dirname, '..');
+  return join(__dirname, '..');
 }
 
 function getRootPackageJson() {
-    return JSON.parse(readFileSync(join(getRootDir(), 'package.json'), { encoding: 'utf8' }));
+  return JSON.parse(readFileSync(join(getRootDir(), 'package.json'), { encoding: 'utf8' }));
 }
 
 function getPackages() {
-    const packagesDir = join(getRootDir(), 'packages');
-    return readdirSync(packagesDir);
+  const packagesDir = join(getRootDir(), 'packages');
+  return readdirSync(packagesDir);
 }
 
 function getPackagesDir() {
-    return join(getRootDir(), 'packages');
+  return join(getRootDir(), 'packages');
 }
 
 function getPackageInfo() {
-    const rootPackJson = getRootPackageJson();
-    const packages = getPackages();
-    const packagesDir = getPackagesDir();
-    
-    return packages.map(pack => {
-        const packJson = JSON.parse(readFileSync(join(packagesDir, pack, 'package.json'), { encoding: 'utf8' }));
-        return { name: packJson.name, version: rootPackJson.version };
-    });
+  const rootPackJson = getRootPackageJson();
+  const packages = getPackages();
+  const packagesDir = getPackagesDir();
+
+  return packages.map((pack) => {
+    const packJson = JSON.parse(
+      readFileSync(join(packagesDir, pack, 'package.json'), { encoding: 'utf8' })
+    );
+    return { name: packJson.name, version: rootPackJson.version };
+  });
 }
 
 function generateTag(version) {
-    const [baseVersion, prerelease] = version.split('-');
-    const versionDigits = baseVersion.replace(/[^0-9]/g, '');
-    const prefix = prerelease ? prerelease.replace(/[^a-zA-Z]/g, '') : 'release';
-    return `${prefix}-${versionDigits}`;
+  const [baseVersion, prerelease] = version.split('-');
+  const versionDigits = baseVersion.replace(/[^0-9]/g, '');
+  const prefix = prerelease ? prerelease.replace(/[^a-zA-Z]/g, '') : 'release';
+  return `${prefix}-${versionDigits}`;
 }
 
 module.exports = {
-    getRootDir,
-    getRootPackageJson,
-    getPackages,
-    getPackagesDir,
-    getPackageInfo,
-    generateTag
+  getRootDir,
+  getRootPackageJson,
+  getPackages,
+  getPackagesDir,
+  getPackageInfo,
+  generateTag
 };
